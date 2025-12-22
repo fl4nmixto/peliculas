19f0b7b379be7b035f6a3bf44d29791958d829fc
Pablo Merener <pablo.merener@alliansys.com.ar>
2025-12-22 09:22:55 -0300

    Add soft deletes to movies

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Movie extends Model
 {
     /** @use HasFactory<\Database\Factories\MovieFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
